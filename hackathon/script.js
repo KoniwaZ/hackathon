c778c762154afe6e49abc925e0382719b2b403f4
KoniwaZ <124517346+KoniwaZ@users.noreply.github.com>
2024-08-03 05:51:29 +0700

    Delete hackathon/script.js

--- hackathon/script.js
+++
@@ -1,4 +0,0 @@
-// Optional: Add any additional interactivity if needed
-document.addEventListener("DOMContentLoaded", function () {
-  // Example: You can add more animations or handle events here
-});
